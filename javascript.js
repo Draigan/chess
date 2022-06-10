4c05f949707ffbb9b39ca0f2d913d7cc39a210c0
Draigan <107204245+Draigan@users.noreply.github.com>
2022-06-10 12:35:34 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -46,6 +46,65 @@ function changeTurn(){
             'encounter':false,
             'pressed': false
         }
+ //      WHAT TURN IS IT?
+
+        var turn = 'white';
+function changeTurn(){
+    
+    if (turn == 'white'){ turn = 'black'; }
+    else{ turn = 'white';}
+    mapImages();
+    lastSquare = ''
+    console.log(lastSquare);
+}
+       
+//      Setting a variable to tell me when we are in a pressed state and the program is checking possibilities
+        var pressed = false;
+
+        //      Current Piece We Have Pressed
+        currentPiece = ''
+
+        //      Last square we were on
+        lastSquare = ''
+
+
+        //    COORDINATE  OBJECTS
+
+        var a8 = {
+            'hasBlackPiece': false,
+            'hasWhitePiece':false,
+            'pieceType': '',
+            'possible':false,
+            'encounter':false,
+           
+        }
+
+        var a7 = {
+            'hasBlackPiece': false,
+            'hasWhitePiece':false,
+            'pieceType': '',
+            'possible':false,
+            'encounter':false,
+            
+        }
+        
+        var a6 = {
+            'hasBlackPiece': true,
+            'hasWhitePiece':false,
+            'pieceType': 'bRook',
+            'possible':false,
+            'encounter':false,
+            
+        }
+
+        var a5 = {
+            'hasBlackPiece': false,
+            'hasWhitePiece':false,
+            'pieceType': '',
+            'possible':false,
+            'encounter':false,
+            
+        }
         
         var a4 = {
             'hasBlackPiece': false,
@@ -53,7 +112,7 @@ function changeTurn(){
             'pieceType': '',
             'possible':false,
             'encounter':false,
-            'pressed': false
+           
         }
 
         var a3 = {
@@ -62,16 +121,17 @@ function changeTurn(){
             'pieceType': '',
             'possible':false,
             'encounter':false,
-            'pressed': false
+           
         }
         
         var a2 = {
             'hasBlackPiece': false,
-            'hasWhitePiece':false,
-            'pieceType': '',
+            'hasWhitePiece':true,
+            'pieceType': 'wRook',
             'possible':false,
             'encounter':false,
-            'pressed': false
+           
+            
         }
 
         var a1 = {
@@ -80,22 +140,29 @@ function changeTurn(){
             'pieceType': '',
             'possible':false,
             'encounter':false,
-            'pressed': false
+            
             
         }
 
 
-        //      Making the squares display the image of the current piece its holding.
+        //      Making the squares display the image of the current piece its holding and showing empty squares
         function mapImages(){
+        
+        //      a2 Empty
+
+        if(lastSquare == 'a2LastSquare') { document.getElementById('a2-id').innerHTML = ""; console.log('iflastsquareworked'); 
+    console.log(a2)}
+        //      White Rook
         if (a1['pieceType'] == 'wRook'){ document.getElementById('a1-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a2['pieceType'] == 'wRook'){ document.getElementById('a2-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
-        if (a3['pieceType'] == 'wRook'){ document.getElementById('a3-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
+        if (a3['pieceType'] == 'wRook'){ document.getElementById("a3-id").innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a4['pieceType'] == 'wRook'){ document.getElementById('a4-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a5['pieceType'] == 'wRook'){ document.getElementById('a5-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a6['pieceType'] == 'wRook'){ document.getElementById('a6-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a7['pieceType'] == 'wRook'){ document.getElementById('a7-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
         if (a8['pieceType'] == 'wRook'){ document.getElementById('a8-id').innerHTML = '<img src="./assets/wR.png" alt="">'; }
-
+       
+        //      Black Rook
         if (a1['pieceType'] == 'bRook'){ document.getElementById('a1-id').innerHTML = '<img src="./assets/bR.png" alt="">'; }
         if (a2['pieceType'] == 'bRook'){ document.getElementById('a2-id').innerHTML = '<img src="./assets/bR.png" alt="">'; }
         if (a3['pieceType'] == 'bRook'){ document.getElementById('a3-id').innerHTML = '<img src="./assets/bR.png" alt="">'; }
@@ -105,7 +172,7 @@ function changeTurn(){
         if (a7['pieceType'] == 'bRook'){ document.getElementById('a7-id').innerHTML = '<img src="./assets/bR.png" alt="">'; }
         if (a8['pieceType'] == 'bRook'){ document.getElementById('a8-id').innerHTML = '<img src="./assets/bR.png" alt="">'; }
     }
-   
+   mapImages();
     
          //      Query Selectors for coordinates
          let a1Div = document.querySelector('.a1-Square');
@@ -121,6 +188,83 @@ function changeTurn(){
          //     Event Listeners for first click and second click
 a2Div.addEventListener('click', a2FirstClick);
 a3Div.addEventListener('click', a3SecondClick);
+
+
+        
+
+
+           //   First Step of Clicking a2 Square
+        function a2FirstClick(){
+                //      If its whites turn and the square has a white piece
+            if ( turn == 'white' && a2['hasWhitePiece'] === true){
+
+                //      Acknowledging that a button has been pressed
+                pressed = true;
+
+                //      Acknowledge that a2 was the last square touched
+                lastSquare = 'a2LastSquare'
+                
+            
+                //      Checking Piece type
+            if ( a2['pieceType'] == 'wRook' && turn == 'white') {
+                //      Running the possibility function
+                a2RookPossible()
+
+                //      Acknowledging that its a wRook to the second click input.
+                currentPiece = a2['pieceType'];
+                console.log(currentPiece)
+
+
+            }
+        }
+
+
+                //    1st Step After Clicking Button -  Find The Possibilities For The Rook at a2 and set the Coordinate objects
+        function a2RookPossible(){
+            
+               //       Possible Moves Up For White Rook
+            let up = [a3,a4,a5,a6,a7,a8]
+
+
+            //      Encountering a black piece and returining the function sorta. Return wasnt working fo rme but could have been
+            //      another problem. Should try to use return again.
+            for (let i = 0; i <= 5; i++){ 
+                if (up[i]['hasBlackPiece'] == true ){
+                        up[i]['encounter'] = true; 
+                        console.log(up[i])
+                       
+                        i = 6
+               
+                    
+                }
+            //      Encountering an empty square
+                else  if (up[i]['hasBlackPiece'] == false && up[i]['hasWhitePiece'] == false ){
+                    up[i]['possible'] = true;
+                    console.log(up[i])
+                
+
+                } 
+
+            }
+
+        }
+            
+        }
+
+        //      
+
+        //      Moving The Piece From a2 SECOND STEP
+        function a3SecondClick(){
+            if (pressed = true && a3['possible'] == true){
+                a3['pieceType'] = currentPiece;
+                a3['hasWhitePiece'] = true;
+                a2['pieceType'] = '';
+                a2['hasWhitePiece'] = false;
+                console.log(a3);
+               
+                changeTurn();
+            } 
+        }
 
         //      Setting a variable to tell me when we are in a pressed state and the program is checking possibilities
         var pressed = false;
